6fdf270557efe40fdadafe225b5681970f7a4006
Michiel Scholten <michiel@aquariusoft.org>
2014-04-11 15:18:58 +0200

    ddwrt_traffic.py only contains functions now (library); added ddwrt2csv.py to write csv output

---
+++ ddwrt2csv.py
@@ -0,0 +1,21 @@
+import csv
+import ddwrt_traffic
+
+file_source = 'traffdata.bak'
+file_csv_permonth = 'traffdata_monthly.csv'
+file_csv_perday = 'traffdata_daily.csv'
+
+with open(file_source) as f_src:
+    content = f_src.readlines()
+    result, result_per_month = ddwrt_traffic.parse_traffdata(content)
+    result = ddwrt_traffic.as_flat_list(result)
+
+    with open(file_csv_permonth, 'wb') as f_csv:
+        writer = csv.writer(f_csv, delimiter=';', quoting=csv.QUOTE_NONE)
+        writer.writerow(['month', 'download', 'upload'])
+        writer.writerows(result_per_month)
+
+    with open(file_csv_perday, 'wb') as f_csv:
+        writer = csv.writer(f_csv, delimiter=';', quoting=csv.QUOTE_NONE)
+        writer.writerow(['date', 'download', 'upload'])
+        writer.writerows(result)
--- ddwrt_traffic.py
+++ ddwrt_traffic.py
@@ -1,4 +1,8 @@
 def formatline(traffline):
+    """
+    Parse line of format:
+    traff-<mm>-<yyyy>=<download_day1>:<upload_day1> <download_day2>:<upload_day2> ... <download_dayN>:<upload_dayN> [<download_month>:<upload_month>]
+    """
     line_data = traffline.strip().split('=')
     line_result = []
     day_counter = 0
@@ -11,12 +15,14 @@ def formatline(traffline):
         day_counter += 1
         day = "{0:0{1}d}".format(day_counter, 2)
         down, up = day_data.split(':')
-        #line_result.append('{0}-{1}-{2} {3} {4}'.format(year, month, day, down, up))
         line_result.append(['{0}-{1}-{2}'.format(year, month, day), down, up])
     return '{0}-{1}'.format(year, month), month_down, month_up, line_result
 
 
 def parse_traffdata(content):
+    """
+    Parse the lines from a dd-wrt WAN traffic overview export
+    """
     result = []
     result_per_month = []
     for traffline in content:
@@ -25,12 +31,14 @@ def parse_traffdata(content):
         month, month_down, month_up, line_result = formatline(traffline)
         result.append(line_result)
         result_per_month.append([month, month_down, month_up])
-    return result, result_per_month
+    return result, sorted(result_per_month, key=lambda month: month[0])
 
 
-filename = 'traffdata.bak'
-with open(filename) as f:
-    content = f.readlines()
-    result, result_per_month = parse_traffdata(content)
-    print(result)
-    print(result_per_month)
+def as_flat_list(all_days):
+    """
+    Flatten the hierarchical list result from the per-month grouped daily data to a flat list
+    """
+    result = []
+    for month in all_days:
+        result.extend(month)
+    return sorted(result, key=lambda day: day[0])

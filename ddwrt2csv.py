12496e9426b32b8faf6886c70249ac6064331b6b
noliono <noliono@gmail.com>
2021-04-10 18:19:59 +0200

    Update python3

--- ddwrt2csv.py
+++ ddwrt2csv.py
@@ -9,13 +9,14 @@ with open(file_source) as f_src:
     content = f_src.readlines()
     result, result_per_month = ddwrt_traffic.parse_traffdata(content)
     result = ddwrt_traffic.as_flat_list(result)
+    #print(result)
 
-    with open(file_csv_permonth, 'wb') as f_csv:
+    with open(file_csv_permonth, 'w', newline='') as f_csv:
         writer = csv.writer(f_csv, delimiter=';', quoting=csv.QUOTE_NONE)
         writer.writerow(['month', 'download', 'upload'])
         writer.writerows(result_per_month)
 
-    with open(file_csv_perday, 'wb') as f_csv:
+    with open(file_csv_perday, 'w', newline='') as f_csv:
         writer = csv.writer(f_csv, delimiter=';', quoting=csv.QUOTE_NONE)
         writer.writerow(['date', 'download', 'upload'])
         writer.writerows(result)
